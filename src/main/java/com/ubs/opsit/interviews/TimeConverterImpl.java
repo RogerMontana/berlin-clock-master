d74e1c81f1bbc97699507d55e38a2311ef600ebe
akarpov <akarpov@playtika.com>
2019-02-09 10:10:37 +0200

    refactored a bit

--- src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -2,6 +2,7 @@ package com.ubs.opsit.interviews;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 
 import com.ubs.opsit.interviews.clock.BerlinClock;
 
@@ -15,7 +16,7 @@ public class TimeConverterImpl implements TimeConverter {
         LocalTime time = LocalTime.parse(input, format);
 
         //workaround to pass 24:00:00 Scenario
-        int hours = input.split(":")[0].equals("24") ? 24 : time.getHour();
+        int hours = input.split(":")[0].equals("24") ? time.get(ChronoField.CLOCK_HOUR_OF_DAY) : time.getHour();
         int minutes = time.getMinute();
         int seconds = time.getSecond();
 
